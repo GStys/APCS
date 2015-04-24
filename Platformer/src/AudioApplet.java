85c6770ee87934d67041a65be0829a6ef3ea793c
GStys <spongebobg@gmail.com>
2015-04-24 10:52:02 -0700

    AudioApplet

---
+++ Platformer/src/AudioApplet.java
@@ -0,0 +1,13 @@
+import java.applet.*;
+import javax.swing.*;
+import java.io.*;
+import java.net.*;
+
+
+public class AudioApplet extends JApplet {
+	public class Sound {
+		private AudioClip song;
+		private URL songPath;
+		Sound (String filename);
+	}
+}
