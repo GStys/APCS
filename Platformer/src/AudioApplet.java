3cf025319379fd87d56c0427c7a6795e5cb20708
Epsylon <Epsylon@LilEpsylon>
2015-04-29 09:15:52 -0700

    Progress on audio... comp about to die

--- Platformer/src/AudioApplet.java
+++ Platformer/src/AudioApplet.java
@@ -8,10 +8,20 @@ public class AudioApplet extends JApplet {
 	public class Sound {
 		private AudioClip song;
 		private URL songPath;
-		/*Sound (String filename) {
+		Sound (String filename) {
 			try {
 				songPath = new URL(getCodeBase(), filename);
-			}
-		}*/
-	}
+				song = Applet.newAudioClip(songPath);
+			}catch(Exception e){}
+		}
+		public void playSound(){
+			song.loop();
+		}
+		public void stopSound(){
+			song.stop();
+		}
+		public void playSoundOnce(){
+			song.play();
+		}
+	}//test change
 }
